e69d0bf89dcc713a64e8e6c4bd0d6ce996697556
Muhammad Rizki Haikal <mrizhs294@gmail.com>
2026-09-07 18:47:59 +0700

    update cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,14 +37,15 @@ export default function Hero() {
           className="flex gap-4 mt-unit"
         >
           <a
-            href="https://drive.google.com/file/d/1PNOIJNoHQJ-GiiQS4_F1xBaCFXLXnTxR/view?usp=drivesdk
-"
+            href="https://drive.google.com/file/d/1E5us_g-j9xcnPz1tF72jZv1dYXuQDQmU/view?usp=drivesdk"
+            target="_blank"
             className="inline-flex items-center justify-center px-6 py-3 bg-primary-container text-on-primary font-label-md text-label-md rounded-lg hover:opacity-90 transition-opacity"
           >
             Unduh CV
           </a>
           <a
             href="https://wa.me/62895334830722"
+            target="_blank"
             className="inline-flex items-center justify-center px-6 py-3 bg-surface-container-lowest text-primary border border-outline-variant font-label-md text-label-md rounded-lg hover:bg-surface-container-low transition-colors"
           >
             Hubungi Saya
